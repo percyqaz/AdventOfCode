c987e0881255283e22fe45df6aa068de27f174be
Percy Qaz <percyqaz@yahoo.co.uk>
2026-06-17 16:59:30 +0100

    2015 Day 24

---
+++ 2015/24/two.py
@@ -0,0 +1,22 @@
+numbers = sorted([int(x) for x in open("input.txt").read().split("\n")])[::-1]
+target = sum(numbers) // 4
+
+def greedy(numbers, current, p, target):
+    if target == 0:
+        yield len(current), p, current
+        return
+
+    ncopy = list(numbers)
+    for n in numbers:
+        ncopy.remove(n)
+        if n <= target:
+            for o in greedy(ncopy, current + [n], p * n, target - n):
+                yield o
+
+group_ones = sorted(list(greedy(numbers, [], 1, target)))
+for _, w, group in group_ones:
+    ncopy = list(numbers)
+    for n in group: ncopy.remove(n)
+    for o in greedy(ncopy, [], 1, target):
+        print(w)
+        exit()
