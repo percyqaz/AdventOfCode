0186fd0512479e0407d911a25841561b5be222eb
Percy Qaz <percyqaz@yahoo.co.uk>
2026-06-17 22:50:41 +0100

    2016 Day 6

---
+++ 2016/06/one.py
@@ -0,0 +1,8 @@
+lines = open("input.txt").read().split("\n")
+l = len(lines[0])
+for i in range(l):
+    count = {}
+    for line in lines:
+        if line[i] not in count: count[line[i]] = 0
+        count[line[i]] += 1
+    print(sorted([(-count[c], c) for c in count])[0][1], end="")
