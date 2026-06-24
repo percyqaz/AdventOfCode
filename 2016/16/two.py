1eef477b8f67f1518d8549f1f637bea5b91e08ee
Percy Qaz <percyqaz@yahoo.co.uk>
2026-06-24 22:51:52 +0100

    2016 Day 16

---
+++ 2016/16/two.py
@@ -0,0 +1,20 @@
+seed = "01110110101001000"
+length = 35651584
+
+def dragon(s):
+    return s + "0" + "".join(["0" if c == "1" else "1" for c in s[::-1]])
+
+def halve(s):
+    result = ""
+    for i in range(0, len(s), 2):
+        if s[i] == s[i + 1]: result += "1"
+        else: result += "0"
+    return result
+
+while len(seed) < length:
+    seed = dragon(seed)
+seed = seed[0:length]
+
+while len(seed) % 2 == 0:
+    seed = halve(seed)
+print(seed)
