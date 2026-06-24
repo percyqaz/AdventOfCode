6409e648a86aa1e6ce707108dcdc3831a72dd98e
Percy Qaz <percyqaz@yahoo.co.uk>
2026-06-24 21:52:04 +0100

    2016 Day 15

---
+++ 2016/15/one.py
@@ -0,0 +1,29 @@
+discs = open("input.txt").read().split("\n")
+
+disc_n = 0
+
+current = 0
+step = 1
+
+def handle_disc(count, rem):
+    global current
+    global step
+    
+    while current % count != rem:
+        current += step
+    
+    step *= count
+
+for disc in discs:
+    disc_n += 1
+    split = disc.strip(".").split()
+    count = int(split[3])
+    init = int(split[-1])
+    
+    rem = -(init + disc_n) % count
+    
+    print("i mod %s = %s" % (count, rem))
+    
+    handle_disc(count, rem)
+    
+print(current)
